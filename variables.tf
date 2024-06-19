43f900f34fb94734f27da777949515b95144d18f
BHarish07 <harishbalike1995@gmail.com>
2024-06-19 09:45:42 +0530

    frontend-deploy

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "frontend"
+  }
+}
+
+variable "zone_name" {
+  default = "harishbalike.online"
+}
+variable "app_version" {
+  
+}
